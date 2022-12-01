e6f99bf69559e3ed1e2698e71e82489a606deaf0
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-12-01 10:47:09 +0530

    commit code

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "development") {
+  console.log("check env...");
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello express");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started on port ${PORT}`);
+});
+
+// const http = require("http");
+
+// const hostname = "local";
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
